0e96dfd3d382e751bb5477cebf9082ea276ec6fe
ervanervan <ervanervan040@gmail.com>
2023-12-18 18:40:34 +0700

    Set up project weserve

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline font-clash-display font-bold">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl underline font-poppins font-bold">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
