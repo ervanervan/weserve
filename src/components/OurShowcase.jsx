eeb3e273d8788dd204cb68869f1dcaff0ba6bc78
ervanervan <ervanervan040@gmail.com>
2023-12-19 16:23:30 +0700

    Finished update component ourshowcase

--- src/components/OurShowcase.jsx
+++ src/components/OurShowcase.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function OurShowcase() {
-  return <section className="py-20">OurShowcase</section>;
+  return <section className="py-20 w-full">OurShowcase</section>;
 }
