2bff1fdb65727ac6852d487a4c7eac8dc5afcd6e
ervanervan <ervanervan040@gmail.com>
2023-12-19 20:09:52 +0700

    Finished creating a responsive brands component

---
+++ src/Data.jsx
@@ -0,0 +1,38 @@
+import imgBrand1 from "./assets/brand-1.svg";
+import imgBrand2 from "./assets/brand-2.svg";
+import imgBrand3 from "./assets/brand-3.svg";
+import imgBrand4 from "./assets/brand-4.svg";
+import imgBrand5 from "./assets/brand-5.svg";
+
+export const DATA_BRANDS = [
+  {
+    id: 1,
+    name: "Brand 1",
+    imgURL: imgBrand1,
+    width: 141,
+  },
+  {
+    id: 2,
+    name: "Brand 2",
+    imgURL: imgBrand2,
+    width: 141,
+  },
+  {
+    id: 3,
+    name: "Brand 3",
+    imgURL: imgBrand3,
+    width: 141,
+  },
+  {
+    id: 4,
+    name: "Brand 4",
+    imgURL: imgBrand4,
+    width: 141,
+  },
+  {
+    id: 5,
+    name: "Brand 5",
+    imgURL: imgBrand5,
+    width: 141,
+  },
+];
--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { DATA_BRANDS } from "../Data";
+import Marquee from "react-fast-marquee";
 
 export default function Brands() {
-  return <section className="py-20">Brands</section>;
+  return (
+    <section className="py-8 my-4">
+      <Marquee autoFill={true} gradient={true} gradientColor="#F1F4F5">
+        {DATA_BRANDS.map((item) => (
+          <div key={item.name} className="flex items-center px-6 md:my-9">
+            <img src={item.imgURL} alt={item.name} width={item.width} />
+          </div>
+        ))}
+      </Marquee>
+    </section>
+  );
 }
