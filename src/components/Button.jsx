d19a6448f7ea22a3126f7e1d9261599db035aecf
ervanervan <ervanervan040@gmail.com>
2023-12-20 10:24:16 +0700

    Finished creating a responsive feature and cardfeatured component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,21 +9,17 @@ export default function Button(props) {
       ${props.className && props.className}
       ${
         props.isPrimary &&
-        "px-6 py-4 lg:px-10 text-white-1 text-sm md:text-base lg:text-lg font-bold bg-blue-2 hover:bg-blue-2-hover transition-all duration-300"
+        "px-6 py-4 lg:px-10 text-white-1 text-sm md:text-base lg:text-lg font-bold bg-blue-2 hover:bg-blue-2-hover"
       }
       ${
         props.isBlue &&
-        "px-7 py-3 text-white-1 text-base font-semibold bg-blue-3 hover:bg-blue-3-hover transition-all duration-300"
+        "px-7 py-3 text-white-1 text-base font-semibold bg-blue-3 hover:bg-blue-3-hover"
       }
       ${
         props.isYellow &&
-        "px-6 py-4 lg:px-10 text-blue-3 text-lg font-bold bg-yellow hover:bg-yellow-hover transition-all duration-300"
+        "px-6 py-4 lg:px-10 text-blue-3 text-lg font-bold bg-yellow hover:bg-yellow-hover"
       }
-      ${
-        props.isWhite &&
-        "text-blue-2 text-base font-semibold bg-white-1 transition-all duration-300"
-      }
-      rounded-full`}
+      rounded-full transition-all duration-300`}
     >
       {props.children}
     </button>
--- src/components/CardFeatured.jsx
+++ src/components/CardFeatured.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "./Button";
 
 export default function CardFeatured({ icon, iconName, title, description }) {
   return (
-    <div className="max-w-[18.50rem] rounded-2xl bg-white-1 px-6 py-8">
-      <div className="flex flex-col gap-y-8">
-        <img src={icon} alt={iconName} width={46} height={46} />
-        <div>
-          <h3 className="text-blue-3 font-bold text-xl mb-1">{title}</h3>
-          <p className="w-4/5 md:w-full">{description}</p>
+    <div className="group">
+      <div className="max-w-[18.50rem] rounded-2xl bg-white-1 group-hover:bg-blue-3 px-6 py-8 transition-all duration-500">
+        <div className="flex flex-col gap-y-8">
+          <img src={icon} alt={iconName} width={46} height={46} />
+          <div>
+            <h3 className="text-blue-3 font-bold text-xl group-hover:text-white-1 mb-1">
+              {title}
+            </h3>
+            <p className="w-4/5 md:w-full group-hover:text-gray-1">
+              {description}
+            </p>
+          </div>
+          <Link to={"/"}>
+            <button className="text-blue-2 bg-transparent text-base font-semibold bg-white-1 rounded-full transition-all duration-300 group-hover:text-white-1">
+              Learn More
+            </button>
+          </Link>
         </div>
-        <Link to={"/"}>
-          <Button isWhite>Learn More</Button>
-        </Link>
       </div>
     </div>
   );
