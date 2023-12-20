a3cc7defeb75a878b43d28bd3b5fbb072bbe96d1
ervanervan <ervanervan040@gmail.com>
2023-12-20 09:12:08 +0700

    Update gradientWidth marquee

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,7 +5,12 @@ import Marquee from "react-fast-marquee";
 export default function Brands() {
   return (
     <section className="py-8 my-4">
-      <Marquee autoFill={true} gradient={true} gradientColor="#F1F4F5">
+      <Marquee
+        autoFill={true}
+        gradient={true}
+        gradientColor="#F1F4F5"
+        gradientWidth={100}
+      >
         {DATA_BRANDS.map((item) => (
           <div key={item.name} className="flex items-center px-6 md:my-9">
             <img src={item.imgURL} alt={item.name} width={item.width} />
