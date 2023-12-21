cc480bff37c749efefb6ce2dc547b57dac74d426
ervanervan <ervanervan040@gmail.com>
2023-12-21 19:52:34 +0700

    Finished creating a responsive FAQ and component

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { DATA_LEFT_FAQ, DATA_RIGHT_FAQ } from "../Data";
 
-import iconArrowDown from "../assets/icon-arrow-down.svg";
 import iconArrowUp from "../assets/icon-arrow-up.svg";
+import iconArrowDown from "../assets/icon-arrow-down.svg";
 import iconDeviceMessageWhite from "../assets/icon-device-message-white.svg";
-import { Link } from "react-router-dom";
 
 export default function FAQ() {
   const [openIdLeft, setOpenIdLeft] = useState(null);
@@ -24,15 +24,17 @@ export default function FAQ() {
         <h2 className="text-center text-[1.75rem] sm:text-3xl md:text-4xl lg:lg:text-[2.50rem] xl:text-5xl">
           Frequently Asked Questions
         </h2>
-        <div className="flex flex-col lg:flex-row justify-between gap-8">
-          <div className="w-1/2 flex flex-col gap-5">
+        <div className="flex flex-col lg:flex-row justify-between gap-5 lg:gap-8">
+          <div className="w-full lg:w-1/2 flex flex-col gap-5">
             {DATA_LEFT_FAQ.map((item) => (
               <div key={item.id} className="bg-white-1 rounded-2xl">
                 <div
-                  className="flex justify-between items-center p-5 cursor-pointer"
+                  className="flex justify-between items-center gap-x-2.5 md:gap-x-0 p-5 cursor-pointer"
                   onClick={() => toggleAccordionLeft(item.id)}
                 >
-                  <h2 className="text-lg font-semibold">{item.question}</h2>
+                  <h3 className="text-blue-3 text-lg md:text-xl font-semibold">
+                    {item.question}
+                  </h3>
                   {openIdLeft === item.id ? (
                     <div className="bg-blue-3 p-1.5 rounded-full">
                       <img
@@ -55,7 +57,7 @@ export default function FAQ() {
                 </div>
                 {openIdLeft === item.id && (
                   <div className="flex flex-col gap-y-5 px-5 pb-5">
-                    <p className="text-gray-700">{item.answer}</p>
+                    <p className="text-gray-2">{item.answer}</p>
                     <Link to={"/"} className="w-fit">
                       <button className="text-blue-2 text-base font-semibold bg-transparent bg-white-1 rounded-full transition-all duration-300 group-hover:text-white-1">
                         Learn More
@@ -66,14 +68,16 @@ export default function FAQ() {
               </div>
             ))}
           </div>
-          <div className="w-1/2 flex flex-col gap-5">
+          <div className="w-full lg:w-1/2 flex flex-col gap-5">
             {DATA_RIGHT_FAQ.map((item) => (
               <div key={item.id} className="bg-white-1 rounded-2xl">
                 <div
-                  className="flex justify-between items-center p-5 cursor-pointer"
+                  className="flex justify-between items-center gap-x-2.5 md:gap-x-0 p-5 cursor-pointer"
                   onClick={() => toggleAccordionRight(item.id)}
                 >
-                  <h2 className="text-lg font-semibold">{item.question}</h2>
+                  <h3 className="text-blue-3 text-lg md:text-xl font-semibold">
+                    {item.question}
+                  </h3>
                   {openIdRight === item.id ? (
                     <div className="bg-blue-3 p-1.5 rounded-full">
                       <img
@@ -96,7 +100,7 @@ export default function FAQ() {
                 </div>
                 {openIdRight === item.id && (
                   <div className="flex flex-col gap-y-5 px-5 pb-5">
-                    <p className="text-gray-700">{item.answer}</p>
+                    <p className="text-gray-2">{item.answer}</p>
                     <Link to={"/"} className="w-fit">
                       <button className="text-blue-2 text-base font-semibold bg-transparent bg-white-1 rounded-full transition-all duration-300 group-hover:text-white-1">
                         Learn More
