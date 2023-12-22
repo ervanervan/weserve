dfba61f37318c9d789f7a9d8ac4874de6d6885ea
ervanervan <ervanervan040@gmail.com>
2023-12-22 16:04:19 +0700

    Finished creating a responsive GetStarted component

--- src/components/CardGetStarted.jsx
+++ src/components/CardGetStarted.jsx
@@ -5,8 +5,8 @@ import shadowMD from "../assets/icon-shadow-MD.svg";
 export default function CardGetStarted({ icon, iconName, iconHover, title }) {
   return (
     <div className="group relative">
-      <div className="w-[8.50rem] sm:w-36 md:w-[10.25rem] md:h-44 bg-white-1 py-7 px-4 rounded-3xl transition-all duration-500 group-hover:bg-blue-3">
-        <div className="flex flex-col items-center justify-center gap-y-5">
+      <div className="w-[8.50rem] sm:w-36 md:w-[10.25rem] md:h-44 flex items-center justify-center bg-white-1 py-7 px-4 rounded-3xl transition-all duration-500 group-hover:bg-blue-3">
+        <div className="flex flex-col items-center gap-y-5">
           <img
             src={icon}
             alt={iconName}
