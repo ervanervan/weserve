d4832b25008fc59052b96a98b3fb744d36458a0a
ervanervan <ervanervan040@gmail.com>
2023-12-22 15:40:10 +0700

    Finished update data, cardgetstarted and getstarted component

--- src/components/CardGetStarted.jsx
+++ src/components/CardGetStarted.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function CardGetStarted({ icon, iconName, iconHover, title }) {
   return (
     <div className="group relative">
-      <div className="w-[8.50rem] md:w-[10.25rem] md:h-44 bg-white-1 py-7 px-4 rounded-3xl transition-all duration-500 group-hover:bg-blue-3">
+      <div className="w-[8.50rem] sm:w-36 md:w-[10.25rem] md:h-44 bg-white-1 py-7 px-4 rounded-3xl transition-all duration-500 group-hover:bg-blue-3">
         <div className="flex flex-col items-center justify-center gap-y-5">
           <img
             src={icon}
