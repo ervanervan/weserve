62dcd65524c09329b2e1d0b7198425e676f66cb0
ervanervan <ervanervan040@gmail.com>
2023-12-22 18:33:37 +0700

    Finished creating the Weserve website

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -10,7 +10,7 @@ import CardGetStarted from "./CardGetStarted";
 export default function GetStarted() {
   return (
     <section className="relative py-8 md:py-12">
-      <div className="flex flex-col xl:flex-row gap-y-16 items-center justify-between px-5 py-7 md:p-12 rounded-3xl bg-blue-2">
+      <div className="flex flex-col lg:flex-row gap-y-16 items-center justify-between px-5 py-7 md:p-12 rounded-3xl bg-blue-2">
         <div className="flex flex-col gap-y-10">
           <div className="bg-white-1 w-fit flex items-center gap-x-2 px-4 py-2 rounded-full">
             <img
@@ -51,7 +51,7 @@ export default function GetStarted() {
           </div>
         </div>
 
-        <div className="w-full lg:w-4/5 xl:w-1/2 flex justify-center xl:justify-end 2xl:ml-72 gap-5 flex-wrap">
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end 2xl:ml-72 gap-5 flex-wrap">
           {DATA_GETSTARTED.map((item) => {
             return <CardGetStarted key={item.id} {...item} />;
           })}
